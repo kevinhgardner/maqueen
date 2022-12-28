e1b139ab97c9f0edcdc8b3f5a2ff019fc0c01709
kevinhgardner <kevinhgardner@gmail.com>
2022-12-28 18:36:55 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,39 +1,87 @@
+function doOff () {
+    if (State != 1) {
+        State = 0
+        led.unplot(0, 0)
+        strip.clear()
+        strip.show()
+    }
+}
 IR.IR_callbackUser(function (message) {
     Lastmessage = message
 })
+function doD () {
+    if (State == 1) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+        basic.pause(100)
+        maqueen.motorStop(maqueen.Motors.All)
+    }
+}
+function doB () {
+    if (State == 1) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
+        basic.pause(100)
+        maqueen.motorStop(maqueen.Motors.All)
+    }
+}
+function doOn () {
+    if (State != 1) {
+        State = 1
+        led.plot(0, 0)
+        strip.showRainbow(1, 360)
+    }
+}
+function doA () {
+    if (State == 1) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+        basic.pause(100)
+        maqueen.motorStop(maqueen.Motors.All)
+    }
+}
+function doC () {
+    if (State == 1) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
+        basic.pause(100)
+        maqueen.motorStop(maqueen.Motors.All)
+    }
+}
 let Button2 = 0
+let strip: neopixel.Strip = null
+let State = 0
 let Lastmessage = 0
 let Busy = 0
 Lastmessage = 0
 let Action = "."
-let State = 0
+State = 0
 led.unplot(0, 0)
-let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
+strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 loops.everyInterval(100, function () {
-    if (Action == "On") {
-        State = 1
-    } else if (Action == "Off") {
-        State = 0
-    }
-    if (true) {
-    	
-    }
-})
-loops.everyInterval(100, function () {
-    if (State == 1) {
-        led.plot(0, 0)
-        strip.showRainbow(1, 360)
-    } else {
-        led.unplot(0, 0)
-        strip.clear()
-        strip.show()
+    if (Action != ".") {
+        music.playTone(262, music.beat(BeatFraction.Whole))
+        if (Action == "On") {
+            doOn()
+        } else if (Action == "Off") {
+            doOff()
+        } else if (Action == "A") {
+            doA()
+        } else if (Action == "B") {
+            doB()
+        } else if (Action == "C") {
+            doC()
+        } else if (Action == "D") {
+            doD()
+        }
+        Action = "."
+        led.unplot(4, 0)
     }
 })
 loops.everyInterval(100, function () {
     if (Busy == 0 && Lastmessage != 0) {
         Busy = 1
         led.plot(4, 0)
-        music.playTone(262, music.beat(BeatFraction.Whole))
         if (Lastmessage == 64) {
             Action = "A"
         } else if (Lastmessage == 9) {
@@ -55,6 +103,6 @@ loops.everyInterval(100, function () {
         Busy = 0
         Lastmessage = 0
     } else {
-        led.unplot(4, 0)
+    	
     }
 })
