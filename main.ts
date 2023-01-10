c50e989e9d6cbabf3e884b09be38b5089dedf33b
kevinhgardner <kevinhgardner@gmail.com>
2023-01-10 21:47:03 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,187 +1,167 @@
-function doMoveLeft (direction: number) {
-    if (direction == 1) {
-        if (motorStateLeft != 1) {
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
-            motorStateLeft = 1
-            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
-        }
-    } else if (direction == -1) {
-        if (motorStateLeft != -1) {
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
-            motorStateLeft = -1
-            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
-        }
-    } else if (direction == 0) {
-        if (motorStateLeft != 0) {
-            maqueen.motorStop(maqueen.Motors.M1)
-            motorStateLeft = 0
-            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
-        }
-    }
+namespace StateKind {
+    export const idle = StateKind.create()
+    export const off = StateKind.create()
+    export const forward = StateKind.create()
+    export const backwards = StateKind.create()
+    export const left = StateKind.create()
+    export const right = StateKind.create()
+    export const leftforward = StateKind.create()
+    export const rightforward = StateKind.create()
+    export const leftbackwards = StateKind.create()
+    export const rightbackwards = StateKind.create()
 }
-function doOff () {
-    if (State != 0) {
-        State = 0
-        led.unplot(2, 2)
-        strip.clear()
-        strip.show()
-    }
-}
-input.onButtonPressed(Button.A, function () {
-    doOn()
-})
-IR.IR_callbackUser(function (message) {
-    Lastmessage = message
-})
-function doD () {
-    if (State == 1) {
-        doMove(-1, -1)
-    }
-}
-function doHandleMessage () {
-    if (Busy == 0 && Lastmessage != 0) {
-        Busy = 1
-        led.plot(4, 0)
-        if (Lastmessage == 64) {
-            Action = "A"
-        } else if (Lastmessage == 9) {
-            Action = "C"
-        } else if (Lastmessage == 7) {
-            Action = "B"
-        } else if (Lastmessage == 25) {
-            Action = "D"
-        } else if (Lastmessage == 69) {
-            Action = "On"
-        } else if (Lastmessage == 71) {
-            Action = "Off"
-        } else if (Lastmessage == 12) {
-            Action = "Bu"
-        } else if (Lastmessage == 94) {
-            Action = "Bd"
-        }
-        Button2 = 0
-        Busy = 0
-        Lastmessage = 0
-    }
-}
-function doB () {
-    if (State == 1) {
-        doMove(0, 1)
-    }
-}
-function doMove (left: number, right: number) {
-    doMoveLeft(left)
-    doMoveRight(right)
-}
-function doOn () {
-    if (State != 1) {
-        State = 1
-        led.plot(2, 2)
-        strip.showRainbow(1, 360)
-    }
-}
-function doBd () {
-	
-}
-input.onButtonPressed(Button.B, function () {
-    doOff()
-})
-function doMoveRight (direction: number) {
-    if (direction == 1) {
-        if (motorStateRight != 1) {
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
-            motorStateRight = 1
-            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
-        }
-    } else if (direction == -1) {
-        if (motorStateRight != -1) {
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
-            motorStateRight = -1
-            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
-        }
-    } else if (direction == 0) {
-        if (motorStateRight != 0) {
-            maqueen.motorStop(maqueen.Motors.M2)
-            motorStateRight = 0
-            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
-        }
-    }
-}
-function doA () {
-    if (State == 1) {
-        if (distance > alertdistance && distance != 0) {
-            doMove(1, 1)
-        }
-    }
-}
-function doBu () {
-	
-}
-function doC () {
-    if (State == 1) {
-        doMove(1, 0)
-    }
-}
-let distance = 0
-let Button2 = 0
-let alertdistance = 0
-let motorStateRight = 0
-let motorStateLeft = 0
-let State = 0
-let Action = ""
-let Lastmessage = 0
-let Busy = 0
-let strip: neopixel.Strip = null
-strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
-strip.clear()
-strip.show()
-Busy = 0
-Lastmessage = 0
-Action = "."
-State = 0
-motorStateLeft = 0
-motorStateRight = 0
-alertdistance = 10
-maqueen.motorStop(maqueen.Motors.All)
-led.unplot(2, 2)
-basic.forever(function () {
-    doHandleMessage()
-})
-loops.everyInterval(100, function () {
-    if (Action != ".") {
-        strip.rotate(1)
-        strip.show()
-        if (Action == "On") {
-            doOn()
-        } else if (Action == "Off") {
-            doOff()
-        } else if (Action == "A") {
-            doA()
-        } else if (Action == "B") {
-            doB()
-        } else if (Action == "C") {
-            doC()
-        } else if (Action == "D") {
-            doD()
-        } else if (Action == "Bu") {
-            doBu()
-        } else if (Action == "Bd") {
-            doBd()
-        }
-        Action = "."
-        led.unplot(4, 0)
+RadioGameController.onButtonPress(RadioGameController.ButtonPins.ButtonA, RadioGameController.ButtonEvents.Down, function () {
+    if (StateMachine.getState() == StateKind.off) {
+        StateMachine.setState(StateKind.idle)
     } else {
-        doMove(0, 0)
+        StateMachine.setState(StateKind.off)
     }
 })
-loops.everyInterval(100, function () {
-    distance = maqueen.Ultrasonic(PingUnit.Centimeters)
-    if (distance <= alertdistance && distance != 0) {
-        if (motorStateLeft == 1) {
-            doMoveLeft(0)
-        }
-        if (motorStateRight == 1) {
-            doMoveRight(0)
+StateMachine.onStateInit(StateKind.rightbackwards, function (oldState) {
+    doMotor(-1, 0)
+    doBottomLeds2(false, false, false, true)
+})
+StateMachine.onStateInit(StateKind.right, function (oldState) {
+    doMotor(1, -1)
+    doBottomLeds2(false, true, false, true)
+})
+function doMotorLeft (num: number) {
+    if (motoron) {
+        if (num == 0) {
+            maqueen.motorStop(maqueen.Motors.M1)
+        } else if (num == 1) {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+        } else if (num == -1) {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
         }
     }
+}
+function doInit () {
+    maqueen.motorStop(maqueen.Motors.All)
+    bottomLeds = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
+    StateMachine.setState(StateKind.off)
+    RadioGameController.enable(123, RadioGameController.Mode.SLAVE)
+    motoron = true
+    maqueen.motorStop(maqueen.Motors.All)
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+}
+StateMachine.onStateInit(StateKind.forward, function (oldState) {
+    doMotor(1, 1)
+    doBottomLeds2(true, true, false, false)
+})
+function doBottomLeds (color: number) {
+    bottomLeds.showColor(color)
+}
+maqueen.ltEvent(maqueen.Patrol1.PatrolRight, maqueen.Voltage.Low, function () {
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+})
+maqueen.ltEvent(maqueen.Patrol1.PatrolRight, maqueen.Voltage.High, function () {
+    if (!(StateMachine.isStateCurrent(StateKind.off))) {
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+    }
+})
+function doMotor (left: number, right: number) {
+    doMotorLeft(left)
+    doMotorRight(right)
+}
+StateMachine.onStateInit(StateKind.idle, function (oldState) {
+    doBottomLeds(neopixel.colors(NeoPixelColors.Green))
+    doMotor(0, 0)
+})
+StateMachine.onStateInit(StateKind.leftforward, function (oldState) {
+    doMotor(0, 1)
+    doBottomLeds2(true, false, false, false)
+})
+maqueen.ltEvent(maqueen.Patrol1.PatrolLeft, maqueen.Voltage.High, function () {
+    if (!(StateMachine.isStateCurrent(StateKind.off))) {
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+    }
+})
+function doMotorRight (num: number) {
+    if (motoron) {
+        if (num == 0) {
+            maqueen.motorStop(maqueen.Motors.M2)
+        } else if (num == 1) {
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+        } else if (num == -1) {
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
+        }
+    }
+}
+StateMachine.onStateInit(StateKind.rightforward, function (oldState) {
+    doMotor(1, 0)
+    doBottomLeds2(false, true, false, false)
+})
+maqueen.ltEvent(maqueen.Patrol1.PatrolLeft, maqueen.Voltage.Low, function () {
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+})
+StateMachine.onStateInit(StateKind.off, function (oldState) {
+    doBottomLeds(neopixel.colors(NeoPixelColors.Black))
+    doMotor(0, 0)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+})
+StateMachine.onStateInit(StateKind.left, function (oldState) {
+    doMotor(-1, 1)
+    doBottomLeds2(true, false, true, false)
+})
+function doBottomLeds2 (leftfront: boolean, rightfront: boolean, leftback: boolean, rightback: boolean) {
+    for (let index = 0; index <= 3; index++) {
+        bottomLeds.setPixelColor(index, neopixel.colors(NeoPixelColors.Black))
+    }
+    if (leftfront) {
+        bottomLeds.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    }
+    if (rightfront) {
+        bottomLeds.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+    }
+    if (leftback) {
+        bottomLeds.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+    }
+    if (rightback) {
+        bottomLeds.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+    }
+    bottomLeds.show()
+}
+StateMachine.onStateInit(StateKind.backwards, function (oldState) {
+    doMotor(-1, -1)
+    doBottomLeds2(false, false, true, true)
+})
+StateMachine.onStateInit(StateKind.leftbackwards, function (oldState) {
+    doMotor(0, -1)
+    doBottomLeds2(false, false, true, false)
+})
+function doUpdateState () {
+    if (StateMachine.getState() != StateKind.off) {
+        if (RadioGameController.buttonIsPressed(RadioGameController.ButtonPins.Up)) {
+            if (RadioGameController.buttonIsPressed(RadioGameController.ButtonPins.Left)) {
+                StateMachine.setState(StateKind.leftforward)
+            } else if (RadioGameController.buttonIsPressed(RadioGameController.ButtonPins.Right)) {
+                StateMachine.setState(StateKind.rightforward)
+            } else {
+                StateMachine.setState(StateKind.forward)
+            }
+        } else if (RadioGameController.buttonIsPressed(RadioGameController.ButtonPins.Down)) {
+            if (RadioGameController.buttonIsPressed(RadioGameController.ButtonPins.Left)) {
+                StateMachine.setState(StateKind.leftbackwards)
+            } else if (RadioGameController.buttonIsPressed(RadioGameController.ButtonPins.Right)) {
+                StateMachine.setState(StateKind.rightbackwards)
+            } else {
+                StateMachine.setState(StateKind.backwards)
+            }
+        } else if (RadioGameController.buttonIsPressed(RadioGameController.ButtonPins.Left)) {
+            StateMachine.setState(StateKind.left)
+        } else if (RadioGameController.buttonIsPressed(RadioGameController.ButtonPins.Right)) {
+            StateMachine.setState(StateKind.right)
+        } else {
+            StateMachine.setState(StateKind.idle)
+        }
+    }
+}
+let bottomLeds: neopixel.Strip = null
+let motoron = false
+doInit()
+loops.everyInterval(100, function () {
+    doUpdateState()
 })
